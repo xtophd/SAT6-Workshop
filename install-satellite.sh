c5f2938e2480e4d1a02076b245772c670c1cfdcd
Christoph Doerbeck <38790538+xtophd@users.noreply.github.com>
2019-01-22 15:59:01 -0600

    Create install-satellite.sh

---
+++ install-satellite.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+## This script is intended to be run:
+##     on the control host (ie: workstation)
+##     CWD =  ~root/SAT6-Workshop
+
+myInventory="./configs/sat6-workshop"
+
+if [ ! -e "${myInventory}" ] ; then
+    echo "ERROR: Are you in the right directory? Can not find ${myInventory}" ; exit
+    exit
+fi
+
+time ansible-playbook -i ${myInventory} -f 5 ./playbooks/satellite-pre-install.yml
+   
+## If previous cmd exited non-zero then exit
+if [ "$?" -ne "0" ] ; then
+    echo "ERROR: previous command exited non-zero, check errors and try again"
+    exit
+fi
+   
+time ansible-playbook -i ${myInventory} -f 5 ./playbooks/satellite-install.yml
