aead2062f3c3cd9c36742a5de9f5ac922a745b54
Christoph Doerbeck <38790538+xtophd@users.noreply.github.com>
2019-02-08 11:04:32 -0600

    Create cheat-ak-create.sh

---
+++ playbooks/templates/cheat-ak-create.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Creating Activation Keys for DEV and PROD"
+
+hammer activation-key create \
+  --name "desktop-dev" \
+  --description "Desktops for development and early testing" \
+  --lifecycle-environment "Desktop-DEV" \
+  --content-view "Desktop-R7"  \
+  --organization "{{ myOrganization }}"
+
+hammer activation-key create \
+  --name "desktop-prob" \
+  --description "Desktops for development and early testing" \
+  --lifecycle-environment "Desktop-PROD" \
+  --content-view "Desktop-R7"  \
+  --organization "{{ myOrganization }}"
